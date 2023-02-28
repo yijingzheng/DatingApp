1f3e87030a41d525cab14fb91fdaca1f6c66fe27
yijingzheng <122712551+yijingzheng@users.noreply.github.com>
2023-02-28 03:09:56 -0800

    wrote server

---
+++ server/src/main/java/constant/Constant.java
@@ -0,0 +1,19 @@
+package constant;
+
+public class Constant {
+    public static final String RES_TYPE = "application/json";
+    public static final String LEFT = "/left/";
+    public static final String RIGHT = "/right/";
+    public static final String WRONG_URL = "wrong url";
+    public static final String INVALID_INPUT = "invalid input";
+    public static final String SUCCEED = "succeed";
+    public static final int POOL_SIZE = 10;
+    public static final String HOST = "localhost";
+    public static final String USER = "user";
+    public static final String PASSWORD = "password";
+    public static final int PORT = 5672;
+    public static final String STATUS_QUEUE = "status_queue";
+    public static final String MATCHES_QUEUE = "matches_queue";
+    public static final String LIKE = "like";
+    public static final String DISLIKE = "dislike";
+}
--- server/src/main/java/servlets/SwipeServlet.java
+++ server/src/main/java/servlets/SwipeServlet.java
@@ -7,16 +7,37 @@ import java.io.IOException;
 
 import com.google.gson.Gson;
 import model.*;
+import rmqpool.*;
+import constant.*;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.concurrent.TimeoutException;
 
 @WebServlet(name = "SwipeServlet", value = "/swipe")
 public class SwipeServlet extends HttpServlet {
 
-    private final String RES_TYPE = "application/json";
-    private final String LEFT = "/left/";
-    private final String RIGHT = "/right/";
-    private final String WRONG_URL = "wrong url";
-    private final String INVALID_INPUT = "invalid input";
     private Gson gson = new Gson();
+    private RMQChannelPool pool;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(Constant.HOST);
+        factory.setPort(Constant.PORT);
+        factory.setUsername(Constant.USER);
+        factory.setPassword(Constant.PASSWORD);
+        try {
+            Connection connection = factory.newConnection();
+            this.pool = new RMQChannelPool(Constant.POOL_SIZE, new RMQChannelFactory(connection));
+        } catch (IOException | TimeoutException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -29,41 +50,56 @@ public class SwipeServlet extends HttpServlet {
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType(RES_TYPE);
-        String msg;
+        response.setContentType(Constant.RES_TYPE);
 
         String url = request.getPathInfo();
 
-        if (!url.equals(LEFT) && !url.equals(RIGHT)) {
+        if (!url.equals(Constant.LEFT) && !url.equals(Constant.RIGHT)) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            msg = this.gson.toJson(new Message(WRONG_URL));
-            response.getWriter().write(msg);
+            response.getWriter().write(Constant.WRONG_URL);
             response.getWriter().flush();
             return;
         }
 
+        String status = url.equals(Constant.LEFT) ? Constant.DISLIKE : Constant.LIKE;
+        StringBuilder sb = new StringBuilder();
+        String s;
+
         try {
-            StringBuilder sb = new StringBuilder();
-            String s;
             while ((s = request.getReader().readLine()) != null) {
                 sb.append(s);
             }
-            SwipeDetails detail = this.gson.fromJson(sb.toString(), SwipeDetails.class);
-
-            if (detail.isValid()) {
-                response.setStatus(HttpServletResponse.SC_OK);
-                msg = this.gson.toJson(detail);
-            } else {
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                msg = this.gson.toJson(INVALID_INPUT);
-            }
-            response.getWriter().write(msg);
-            response.getWriter().flush();
         } catch (Exception e) {
-            e.printStackTrace();
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             response.getWriter().write(e.getMessage());
             response.getWriter().flush();
         }
+
+        SwipeDetails detail = this.gson.fromJson(sb.toString(), SwipeDetails.class);
+
+        if (!detail.isValid()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write(Constant.INVALID_INPUT);
+            response.getWriter().flush();
+        }
+
+        if (pool == null) {
+            System.out.println("why is pool null");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write("why is pool null");
+            return;
+        }
+        Channel channel = pool.borrowObject();
+        channel.queueDeclare(Constant.STATUS_QUEUE, false, false, false, null);
+        channel.queueDeclare(Constant.MATCHES_QUEUE, false, false, false, null);
+        channel.basicPublish("", Constant.STATUS_QUEUE, null, status.getBytes());
+        if (status.equals(Constant.LIKE)) {
+            channel.basicPublish("", Constant.MATCHES_QUEUE, null, this.gson.toJson(detail).toString().getBytes());
+        }
+        pool.returnObject(channel);
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write(Constant.SUCCEED);
+        response.getWriter().flush();
     }
 }
